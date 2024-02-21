fff6b1ad98d2d8c7f5d3808c3416c869430b2006
maulana677 <maull0776@gmail.com>
2024-02-21 15:40:53 +0700

    update user admin

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all('roles', 'pengguna');
+        $users = User::all();
 
         return view('admin.pages.user.index', compact('users'));
     }
