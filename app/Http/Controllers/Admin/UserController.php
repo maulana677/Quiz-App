449ef9cdeb4904523bb89315205168b87a0306f2
maulana677 <maull0776@gmail.com>
2024-02-21 15:05:31 +0700

    update logout frontend

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::where('roles', 'pengguna')->get();
+        $users = User::all('roles', 'pengguna');
 
         return view('admin.pages.user.index', compact('users'));
     }
--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -11,6 +11,47 @@
 </head>
 
 <body class="bg-soft-blue">
+    <nav class="navbar navbar-expand-lg py-3 bg-white">
+        <div class="container">
+            <a class="navbar-brand logo" href="{{ route('home') }}">
+                <img src="assets/images/logo.png" alt="Logo">
+                <h5 class="text-dark fw-bold mb-0">Quizz</h5>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+
+                </ul>
+                <ul class="navbar-nav">
+                    <ul class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="true">
+                            {{ auth()->user()->name }}
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-end border mt-3" data-bs-popper="static">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <div class="container py-5">
         <nav class="d-flex justify-content-center">
             <a href="." class="logo">
